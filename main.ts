8be08b63c913be9717320b604b2fd4d7df463eac
ottodmatic <156123287+ottodmatic@users.noreply.github.com>
2024-01-11 13:09:41 -0600

    made tile map

--- main.ts
+++ main.ts
@@ -1,1 +1,43 @@
-
+let myMinimap: minimap.Minimap = null
+let mySprite = sprites.create(img`
+    . . . . f f f f . . . . 
+    . . f f e e e e f f . . 
+    . f f e e e e e e f f . 
+    f f f f 4 e e e f f f f 
+    f f f 4 4 4 e e f f f f 
+    f f f 4 4 4 4 e e f f f 
+    f 4 e 4 4 4 4 4 4 e 4 f 
+    f 4 4 f f 4 4 f f 4 4 f 
+    f e 4 d d d d d d 4 e f 
+    . f e d d b b d d e f . 
+    . f f e 4 4 4 4 e f f . 
+    e 4 f b 1 1 1 1 b f 4 e 
+    4 d f 1 1 1 1 1 1 f d 4 
+    4 4 f 6 6 6 6 6 6 f 4 4 
+    . . . f f f f f f . . . 
+    . . . f f . . f f . . . 
+    `, SpriteKind.Player)
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(img`
+    . . . . f f f f . . . . 
+    . . f f e e e e f f . . 
+    . f f e e e e e e f f . 
+    f f f f 4 e e e f f f f 
+    f f f 4 4 4 e e f f f f 
+    f f f 4 4 4 4 e e f f f 
+    f 4 e 4 4 4 4 4 4 e 4 f 
+    f 4 4 f f 4 4 f f 4 4 f 
+    f e 4 d d d d d d 4 e f 
+    . f e d d b b d d e f . 
+    . f f e 4 4 4 4 e f f . 
+    e 4 f b 1 1 1 1 b f 4 e 
+    4 d f 1 1 1 1 1 1 f d 4 
+    4 4 f 6 6 6 6 6 6 f 4 4 
+    . . . f f f f f f . . . 
+    . . . f f . . f f . . . 
+    `, SpriteKind.Player))
+tiles.setCurrentTilemap(tilemap`level1`)
+scene.cameraFollowSprite(mySprite)
+mySprite.setPosition(70, 60)
+game.onUpdateInterval(500, function () {
+    myMinimap = minimap.minimap()
+})
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -3,6 +3,27 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
 
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`0c000c000307070707090707070707050110110b0b0b0b0b0b0b0b060a110d0b0b0b0b0b0b0b0b060110110b0b0b0b0b0b0b0b06010b0b0b0b0b0b0b0b0b0b06010c0c0c0c0c0c0b0b0b0b06010c0c0c0c0c0c0b0b0b0b060e0b0b0b0b0b0b0b0b0b0b06010b0b0b0b0b0b0b0b0b0b06010b0b0b0b0b0b0b0b0b0b06010b0b0b0b0b0b0b0b0b0b0602080808080f0f0808080804`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . 2 
+2 . . . . . . . . . . 2 
+2 . . . . . . . . . . 2 
+2 . . . . . . . . . . 2 
+2 . . . . . . . . . . 2 
+2 . . . . . . . . . . 2 
+2 . . . . . . . . . . 2 
+2 . . . . . . . . . . 2 
+2 . . . . . . . . . . 2 
+2 . . . . . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.dungeon.purpleOuterWest0,sprites.dungeon.purpleOuterSouthEast,sprites.dungeon.purpleOuterNorthWest,sprites.dungeon.purpleOuterSouthWest,sprites.dungeon.purpleOuterNorthEast,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleOuterNorth1,sprites.dungeon.purpleOuterSouth1,sprites.dungeon.purpleOuterNorth2,sprites.dungeon.purpleOuterWest2,sprites.dungeon.darkGroundSouthWest1,sprites.dungeon.darkGroundSouthEast1,sprites.dungeon.stairLarge,sprites.dungeon.purpleOuterWest1,sprites.dungeon.stairNorth,sprites.dungeon.darkGroundNorthWest1,sprites.dungeon.floorDark0], TileScale.Sixteen);
+        }
+        return null;
+    })
+
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
