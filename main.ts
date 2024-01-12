c52e8f025202687c7169439983561a470917e722
ottodmatic <156123287+ottodmatic@users.noreply.github.com>
2024-01-12 16:08:28 -0600

    made it more organized, fixed the issue with player 2 spawning

--- main.ts
+++ main.ts
@@ -1,54 +1,51 @@
-controller.player2.onEvent(ControllerEvent.Connected, function () {
-    controller.player2.moveSprite(play2, 100, 100)
-})
-controller.player1.onEvent(ControllerEvent.Connected, function () {
-    controller.player1.moveSprite(play1, 100, 100)
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    play2 = sprites.create(img`
+        . . . . f f f f . . . . . 
+        . . f f f f f f f f . . . 
+        . f f f f f f c f f f . . 
+        f f f f f f c c f f f c . 
+        f f f c f f f f f f f c . 
+        c c c f f f e e f f c c . 
+        f f f f f e e f f c c f . 
+        f f f b f e e f b f f f . 
+        . f 4 1 f 4 4 f 1 4 f . . 
+        . f e 4 4 4 4 4 4 e f . . 
+        . f f f e e e e f f f . . 
+        f e f b 7 7 7 7 b f e f . 
+        e 4 f 7 7 7 7 7 7 f 4 e . 
+        e e f 6 6 6 6 6 6 f e e . 
+        . . . f f f f f f . . . . 
+        . . . f f . . f f . . . . 
+        `, SpriteKind.Player)
+    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(img`
+        . . . . f f f f . . . . . 
+        . . f f f f f f f f . . . 
+        . f f f f f f c f f f . . 
+        f f f f f f c c f f f c . 
+        f f f c f f f f f f f c . 
+        c c c f f f e e f f c c . 
+        f f f f f e e f f c c f . 
+        f f f b f e e f b f f f . 
+        . f 4 1 f 4 4 f 1 4 f . . 
+        . f e 4 4 4 4 4 4 e f . . 
+        . f f f e e e e f f f . . 
+        f e f b 7 7 7 7 b f e f . 
+        e 4 f 7 7 7 7 7 7 f 4 e . 
+        e e f 6 6 6 6 6 6 f e e . 
+        . . . f f f f f f . . . . 
+        . . . f f . . f f . . . . 
+        `, SpriteKind.Player))
+    play2.setPosition(play1.x, play1.y)
+    scene.cameraFollowSprite(play2)
+    health_2 = statusbars.create(20, 4, StatusBarKind.Health)
+    health_2.attachToSprite(play2)
 })
 let myMinimap: minimap.Minimap = null
-let play1: Sprite = null
+let health_2: StatusBarSprite = null
 let play2: Sprite = null
-let playbar = statusbars.create(20, 4, StatusBarKind.Health)
-let play2bar = statusbars.create(20, 4, StatusBarKind.Health)
+let health_1: StatusBarSprite = null
+let play1: Sprite = null
 tiles.setCurrentTilemap(tilemap`level1`)
-play2 = sprites.create(img`
-    . . . . f f f f . . . . . 
-    . . f f f f f f f f . . . 
-    . f f f f f f c f f f . . 
-    f f f f f f c c f f f c . 
-    f f f c f f f f f f f c . 
-    c c c f f f e e f f c c . 
-    f f f f f e e f f c c f . 
-    f f f b f e e f b f f f . 
-    . f 4 1 f 4 4 f 1 4 f . . 
-    . f e 4 4 4 4 4 4 e f . . 
-    . f f f e e e e f f f . . 
-    f e f b 7 7 7 7 b f e f . 
-    e 4 f 7 7 7 7 7 7 f 4 e . 
-    e e f 6 6 6 6 6 6 f e e . 
-    . . . f f f f f f . . . . 
-    . . . f f . . f f . . . . 
-    `, SpriteKind.Player)
-mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(img`
-    . . . . f f f f . . . . . 
-    . . f f f f f f f f . . . 
-    . f f f f f f c f f f . . 
-    f f f f f f c c f f f c . 
-    f f f c f f f f f f f c . 
-    c c c f f f e e f f c c . 
-    f f f f f e e f f c c f . 
-    f f f b f e e f b f f f . 
-    . f 4 1 f 4 4 f 1 4 f . . 
-    . f e 4 4 4 4 4 4 e f . . 
-    . f f f e e e e f f f . . 
-    f e f b 7 7 7 7 b f e f . 
-    e 4 f 7 7 7 7 7 7 f 4 e . 
-    e e f 6 6 6 6 6 6 f e e . 
-    . . . f f f f f f . . . . 
-    . . . f f . . f f . . . . 
-    `, SpriteKind.Player))
-play2.setPosition(70, 60)
-scene.cameraFollowSprite(play2)
-play2bar.attachToSprite(play2)
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(img`
     . . . . f f f f . . . . 
     . . f f e e e e f f . . 
@@ -87,7 +84,8 @@ play1 = sprites.create(img`
     `, SpriteKind.Player)
 play1.setPosition(80, 60)
 scene.cameraFollowSprite(play1)
-playbar.attachToSprite(play1)
+health_1.attachToSprite(play1)
+health_1 = statusbars.create(20, 4, StatusBarKind.Health)
 game.onUpdateInterval(500, function () {
-    myMinimap = minimap.minimap()
+    myMinimap = minimap.minimap(MinimapScale.Half, 2, 0)
 })
