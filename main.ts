04888cf1d88feba1328ce5e3fbe7306cc4f61995
chino-cochino-9 <156123454+chino-cochino-9@users.noreply.github.com>
2024-01-18 11:25:24 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -58,30 +58,6 @@ function purpleGreen_2 () {
 }
 function lvl_1 () {
     scene.setBackgroundColor(6)
-    play1 = sprites.create(img`
-        . . . . f f f f . . . . 
-        . . f f e e e e f f . . 
-        . f f e e e e e e f f . 
-        f f f f 4 e e e f f f f 
-        f f f 4 4 4 e e f f f f 
-        f f f 4 4 4 4 e e f f f 
-        f 4 e 4 4 4 4 4 4 e 4 f 
-        f 4 4 f f 4 4 f f 4 4 f 
-        f e 4 d d d d d d 4 e f 
-        . f e d d b b d d e f . 
-        . f f e 4 4 4 4 e f f . 
-        e 4 f b 1 1 1 1 b f 4 e 
-        4 d f 1 1 1 1 1 1 f d 4 
-        4 4 f 6 6 6 6 6 6 f 4 4 
-        . . . f f f f f f . . . 
-        . . . f f . . f f . . . 
-        `, SpriteKind.Player)
-    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`play1`, SpriteKind.Player))
-    play1.setPosition(80, 60)
-    scene.cameraFollowSprite(play1)
-    health_1 = statusbars.create(20, 4, StatusBarKind.Health)
-    health_1.attachToSprite(play1)
-    controller.moveSprite(play1, 100, 100)
     tiles.setWallAt(tiles.getTileLocation(11, 6), false)
     lv1en()
 }
@@ -136,12 +112,35 @@ function lv1en () {
     lvl1en.follow(play2, randint(10, 100))
 }
 let lvl1en: Sprite = null
-let health_1: StatusBarSprite = null
 let health_2: StatusBarSprite = null
 let play2: Sprite = null
 let count = 0
-let play1: Sprite = null
 let tileList: tiles.TileMapData[] = []
+let play1: Sprite = null
+play1 = sprites.create(img`
+    . . . . f f f f . . . . 
+    . . f f e e e e f f . . 
+    . f f e e e e e e f f . 
+    f f f f 4 e e e f f f f 
+    f f f 4 4 4 e e f f f f 
+    f f f 4 4 4 4 e e f f f 
+    f 4 e 4 4 4 4 4 4 e 4 f 
+    f 4 4 f f 4 4 f f 4 4 f 
+    f e 4 d d d d d d 4 e f 
+    . f e d d b b d d e f . 
+    . f f e 4 4 4 4 e f f . 
+    e 4 f b 1 1 1 1 b f 4 e 
+    4 d f 1 1 1 1 1 1 f d 4 
+    4 4 f 6 6 6 6 6 6 f 4 4 
+    . . . f f f f f f . . . 
+    . . . f f . . f f . . . 
+    `, SpriteKind.Player)
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`play1`, SpriteKind.Player))
+play1.setPosition(80, 60)
+scene.cameraFollowSprite(play1)
+let health_1 = statusbars.create(20, 4, StatusBarKind.Health)
+health_1.attachToSprite(play1)
+controller.moveSprite(play1, 100, 100)
 let TileMapLevel = 0
 tileList = [
 tilemap`level7`,
