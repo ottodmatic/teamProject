7e2de6de0a917878346cfe34d28568eb097104f8
ottodmatic <156123287+ottodmatic@users.noreply.github.com>
2024-01-19 12:22:59 -0600

    worked on some glitches and errors

--- main.ts
+++ main.ts
@@ -6,7 +6,9 @@ function Grey () {
     tiles.setWallAt(tiles.getTileLocation(11, 5), false)
 }
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairNorth, function (sprite, location) {
-    tiles.setCurrentTilemap(tileList.removeAt(randint(0, tileList.length - 1)))
+    if (count < 5) {
+        tiles.setCurrentTilemap(tileList.removeAt(randint(0, tileList.length - 1)))
+    }
     count += 1
 })
 function purpleGreen_1 () {
@@ -15,40 +17,21 @@ function purpleGreen_1 () {
     tiles.setWallAt(tiles.getTileLocation(0, 4), false)
 }
 controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
-    play2 = sprites.create(img`
-        . . . . f f f f . . . . . 
-        . . f f f f f f f f . . . 
-        . f f f f f f c f f f . . 
-        f f f f f f c c f f f c . 
-        f f f c f f f f f f f c . 
-        c c c f f f e e f f c c . 
-        f f f f f e e f f c c f . 
-        f f f b f e e f b f f f . 
-        . f 4 1 f 4 4 f 1 4 f . . 
-        . f e 4 4 4 4 4 4 e f . . 
-        . f f f e e e e f f f . . 
-        f e f b 7 7 7 7 b f e f . 
-        e 4 f 7 7 7 7 7 7 f 4 e . 
-        e e f 6 6 6 6 6 6 f e e . 
-        . . . f f f f f f . . . . 
-        . . . f f . . f f . . . . 
-        `, SpriteKind.Player)
-    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), play2)
-    play2.setPosition(play1.x, play1.y)
-    scene.cameraFollowSprite(play2)
-    health_2 = statusbars.create(20, 4, StatusBarKind.Health)
-    health_2.attachToSprite(play2)
-    controller.player2.moveSprite(play2)
+	
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.doorOpenEast, function (sprite, location) {
-    tiles.setCurrentTilemap(tileList.removeAt(randint(0, tileList.length - 1)))
+    if (count < 5) {
+        tiles.setCurrentTilemap(tileList.removeAt(randint(0, tileList.length - 1)))
+    }
     count += 1
 })
 function final () {
 	
 }
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairWest, function (sprite, location) {
-    tiles.setCurrentTilemap(tileList.removeAt(randint(0, tileList.length - 1)))
+    if (count < 5) {
+        tiles.setCurrentTilemap(tileList.removeAt(randint(0, tileList.length - 1)))
+    }
     count += 1
 })
 function purpleGreen_2 () {
@@ -62,7 +45,9 @@ function lvl_1 () {
     lv1en()
 }
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairEast, function (sprite, location) {
-    tiles.setCurrentTilemap(tileList.removeAt(randint(0, tileList.length - 1)))
+    if (count < 5) {
+        tiles.setCurrentTilemap(tileList.removeAt(randint(0, tileList.length - 1)))
+    }
     count += 1
 })
 function purplePurple () {
@@ -108,11 +93,8 @@ function lv1en () {
         `, SpriteKind.Enemy)
     tiles.placeOnTile(lvl1en, tiles.getTileLocation(randint(0, 11), randint(0, 11)))
     lvl1en.follow(play1, randint(10, 100))
-    lvl1en.follow(play2, randint(10, 100))
 }
 let lvl1en: Sprite = null
-let health_2: StatusBarSprite = null
-let play2: Sprite = null
 let count = 0
 let tileList: tiles.TileMapData[] = []
 let play1: Sprite = null
