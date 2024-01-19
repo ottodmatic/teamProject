13100f385213055a50cc544f5b82a540b75a4bbf
ottodmatic <156123287+ottodmatic@users.noreply.github.com>
2024-01-19 16:28:33 -0600

    fixed some stuff, started enemy code

--- main.ts
+++ main.ts
@@ -65,13 +65,39 @@ function purpleGreen_2 () {
 function lvl_1 () {
     scene.setBackgroundColor(6)
     tiles.setWallAt(tiles.getTileLocation(11, 6), false)
-    lv1en()
 }
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairEast, function (sprite, location) {
     if (count < 5) {
         tiles.setCurrentTilemap(tileList.removeAt(randint(0, tileList.length - 1)))
     }
     count += 1
+})
+function enemy () {
+    snake = sprites.create(img`
+        . . . . c c c c c c . . . . . . 
+        . . . c 6 7 7 7 7 6 c . . . . . 
+        . . c 7 7 7 7 7 7 7 7 c . . . . 
+        . c 6 7 7 7 7 7 7 7 7 6 c . . . 
+        . c 7 c 6 6 6 6 c 7 7 7 c . . . 
+        . f 7 6 f 6 6 f 6 7 7 7 f . . . 
+        . f 7 7 7 7 7 7 7 7 7 7 f . . . 
+        . . f 7 7 7 7 6 c 7 7 6 f c . . 
+        . . . f c c c c 7 7 6 f 7 7 c . 
+        . . c 7 2 7 7 7 6 c f 7 7 7 7 c 
+        . c 7 7 2 7 7 c f c 6 7 7 6 c c 
+        c 1 1 1 1 7 6 f c c 6 6 6 c . . 
+        f 1 1 1 1 1 6 6 c 6 6 6 6 f . . 
+        f 6 1 1 1 1 1 6 6 6 6 6 c f . . 
+        . f 6 1 1 1 1 1 1 6 6 6 f . . . 
+        . . c c c c c c c c c f . . . . 
+        `, SpriteKind.Enemy)
+    for (let index = 0; index < randint(1, 2 * count); index++) {
+        tiles.placeOnTile(snake, tiles.getTileLocation(randint(0, 11), randint(0, 11)))
+        snake.follow(play1, randint(10, 50 + 5 * count))
+    }
+}
+tileUtil.onMapLoaded(function (tilemap2) {
+    enemy()
 })
 function purplePurple () {
     play1.setPosition(22, 88)
@@ -95,29 +121,7 @@ tileUtil.onMapLoaded(function (tilemap2) {
         purpleGreen_2()
     }
 })
-function lv1en () {
-    lvl1en = sprites.create(img`
-        . . . . c c c c c c . . . . . . 
-        . . . c 6 7 7 7 7 6 c . . . . . 
-        . . c 7 7 7 7 7 7 7 7 c . . . . 
-        . c 6 7 7 7 7 7 7 7 7 6 c . . . 
-        . c 7 c 6 6 6 6 c 7 7 7 c . . . 
-        . f 7 6 f 6 6 f 6 7 7 7 f . . . 
-        . f 7 7 7 7 7 7 7 7 7 7 f . . . 
-        . . f 7 7 7 7 6 c 7 7 6 f c . . 
-        . . . f c c c c 7 7 6 f 7 7 c . 
-        . . c 7 2 7 7 7 6 c f 7 7 7 7 c 
-        . c 7 7 2 7 7 c f c 6 7 7 6 c c 
-        c 1 1 1 1 7 6 f c c 6 6 6 c . . 
-        f 1 1 1 1 1 6 6 c 6 6 6 6 f . . 
-        f 6 1 1 1 1 1 6 6 6 6 6 c f . . 
-        . f 6 1 1 1 1 1 1 6 6 6 f . . . 
-        . . c c c c c c c c c f . . . . 
-        `, SpriteKind.Enemy)
-    tiles.placeOnTile(lvl1en, tiles.getTileLocation(randint(0, 11), randint(0, 11)))
-    lvl1en.follow(play1, randint(10, 100))
-}
-let lvl1en: Sprite = null
+let snake: Sprite = null
 let health_2: StatusBarSprite = null
 let play2: Sprite = null
 let count = 0
