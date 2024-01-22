6e0c040eaa36522606ad1211d4698edc5b8b340d
chino-cochino-9 <156123454+chino-cochino-9@users.noreply.github.com>
2024-01-22 13:10:37 -0600

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -3,7 +3,128 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-
+            case "~qv,y5CSX#C;du)?UoXn":
+            case "player1":return img`
+. . . . f f f f . . . . 
+. . f f e e e e f f . . 
+. f f e e e e e e f f . 
+f f f f 4 e e e f f f f 
+f f f 4 4 4 e e f f f f 
+f f f 4 4 4 4 e e f f f 
+f 4 e 4 4 4 4 4 4 e 4 f 
+f 4 4 f f 4 4 f f 4 4 f 
+f e 4 d d d d d d 4 e f 
+. f e d d b b d d e f . 
+. f f e 4 4 4 4 e f f . 
+e 4 f b 1 1 1 1 b f 4 e 
+4 d f 1 1 1 1 1 1 f d 4 
+4 4 f 6 6 6 6 6 6 f 4 4 
+. . . f f f f f f . . . 
+. . . f f . . f f . . . 
+`;
+            case "[JpK,Y,3;HJ3N5`KtItN":
+            case "enemy1":return img`
+. . . . c c c c c c . . . . . . 
+. . . c 6 7 7 7 7 6 c . . . . . 
+. . c 7 7 7 7 7 7 7 7 c . . . . 
+. c 6 7 7 7 7 7 7 7 7 6 c . . . 
+. c 7 c 6 6 6 6 c 7 7 7 c . . . 
+. f 7 6 f 6 6 f 6 7 7 7 f . . . 
+. f 7 7 7 7 7 7 7 7 7 7 f . . . 
+. . f 7 7 7 7 6 c 7 7 6 f c . . 
+. . . f c c c c 7 7 6 f 7 7 c . 
+. . c 7 2 7 7 7 6 c f 7 7 7 7 c 
+. c 7 7 2 7 7 c f c 6 7 7 6 c c 
+c 1 1 1 1 7 6 f c c 6 6 6 c . . 
+f 1 1 1 1 1 6 6 c 6 6 6 6 f . . 
+f 6 1 1 1 1 1 6 6 6 6 6 c f . . 
+. f 6 1 1 1 1 1 1 6 6 6 f . . . 
+. . c c c c c c c c c f . . . . 
+`;
+            case "image1":
+            case "purplegreen2":return img`
+........................
+........................
+........................
+........................
+..........ffff..........
+........ff1111ff........
+.......fb111111bf.......
+.......f11111111f.......
+......fd11111111df......
+......fd11111111df......
+......fddd1111dddf......
+......fbdbfddfbdbf......
+......fcdcf11fcdcf......
+.......fb111111bf.......
+......fffcdb1bdffff.....
+....fc111cbfbfc111cf....
+....f1b1b1ffff1b1b1f....
+....fbfbffffffbfbfbf....
+.........ffffff.........
+...........fff..........
+........................
+........................
+........................
+........................
+`;
+            case "image2":
+            case "purpgreen1":return img`
+. . f f f . . . . . . . . f f f 
+. f f c c . . . . . . f c b b c 
+f f c c . . . . . . f c b b c . 
+f c f c . . . . . . f b c c c . 
+f f f c c . c c . f c b b c c . 
+f f c 3 c c 3 c c f b c b b c . 
+f f b 3 b c 3 b c f b c c b c . 
+. c b b b b b b c b b c c c . . 
+. c 1 b b b 1 b b c c c c . . . 
+c b b b b b b b b b c c . . . . 
+c b c b b b c b b b b f . . . . 
+f b 1 f f f 1 b b b b f c . . . 
+f b b b b b b b b b b f c c . . 
+. f b b b b b b b b c f . . . . 
+. . f b b b b b b c f . . . . . 
+. . . f f f f f f f . . . . . . 
+`;
+            case "image3":
+            case "purplepurp":return img`
+. . . . . . . . . . . . . . . . 
+. . . . c c c c . . . . . . . . 
+. . c c 5 5 5 5 c c . . . . . . 
+. c 5 5 5 5 5 5 5 5 c . . . . . 
+c 5 5 5 5 5 1 f 5 5 5 c . . . . 
+c 5 5 5 5 5 f f 5 5 5 5 c . . . 
+c 5 5 5 5 5 5 5 5 5 5 5 c . . . 
+c c b b 1 b 5 5 5 5 5 5 d c . . 
+c 5 3 3 3 5 5 5 5 5 d d d c . . 
+. b 5 5 5 5 5 5 5 5 d d d c . . 
+. . c b b c 5 5 b d d d d c c . 
+. c b b c 5 5 b b d d d d c d c 
+. c c c c c c d d d d d d d d c 
+. . . c c c c d 5 5 b d d d c . 
+. . c c c c c b 5 5 b c c c . . 
+. . c b b b c d 5 5 b c . . . . 
+`;
+            case "image4":
+            case "purpgrey":return img`
+. . . . . . . . b b b b . . . . 
+. . . . b b b b 3 3 3 3 b . . . 
+. c c b b 1 1 3 3 3 3 3 b b . . 
+c c b b 1 1 3 3 3 3 3 1 1 b . . 
+c b b b b b 3 3 3 3 3 1 1 b . . 
+f b b b b b 3 3 3 3 3 3 3 c . . 
+f b b b b b b 3 3 3 3 3 3 b c . 
+f b b b b b b b b 3 3 3 b b b c 
+. c b b b b b b b b b b b b b c 
+. c c b b b b b b 1 1 b b b b f 
+. f d c b b b b b 1 1 b b b b f 
+. f d d c c b b b b b b b b b f 
+. . c d d d c c b b b b b b f f 
+. . . c d d d d c c c c f f f . 
+. . . f c c c f b b b b . . . . 
+. . . f d d d f f . . . . . . . 
+`;
         }
         return null;
     })
--- main.ts
+++ main.ts
@@ -1,6 +1,25 @@
 namespace SpriteKind {
     export const Door = SpriteKind.create()
 }
+tileUtil.onMapLoaded(function (tilemap2) {
+    tileUtil.onMapLoaded(function (tilemap2) {
+    if (tiles.tileAtLocationEquals(tiles.getTileLocation(11, 6), sprites.dungeon.doorOpenEast)) {
+        lvl_1()
+    }
+    if (tiles.tileAtLocationEquals(tiles.getTileLocation(0, 5), sprites.dungeon.stairEast)) {
+        purpleGreen_1()
+    }
+    if (tiles.tileAtLocationEquals(tiles.getTileLocation(5, 11), sprites.dungeon.stairNorth)) {
+        purplePurple()
+    }
+    if (tiles.tileAtLocationEquals(tiles.getTileLocation(11, 5), sprites.dungeon.stairEast)) {
+        Grey()
+    }
+    if (tiles.tileAtLocationEquals(tiles.getTileLocation(11, 6), sprites.dungeon.stairWest)) {
+        purpleGreen_2()
+    }
+})
+})
 function Grey () {
     play1.setPosition(22, 88)
     tiles.setWallAt(tiles.getTileLocation(11, 5), false)
@@ -44,10 +63,9 @@ function purpleGreen_2 () {
     tiles.setWallAt(tiles.getTileLocation(11, 5), false)
 }
 function lvl_1 () {
-    let list: number[] = []
     scene.setBackgroundColor(6)
     tiles.setWallAt(tiles.getTileLocation(11, 6), false)
-    enemy_1(list)
+    enemy_1(enmlist)
 }
 function enemy_1 (enemyList: any[]) {
     enemy()
@@ -60,49 +78,13 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairEast, function (spri
 })
 function enemy () {
     count2 = 0
-    snake = sprites.create(img`
-        . . . . c c c c c c . . . . . . 
-        . . . c 6 7 7 7 7 6 c . . . . . 
-        . . c 7 7 7 7 7 7 7 7 c . . . . 
-        . c 6 7 7 7 7 7 7 7 7 6 c . . . 
-        . c 7 c 6 6 6 6 c 7 7 7 c . . . 
-        . f 7 6 f 6 6 f 6 7 7 7 f . . . 
-        . f 7 7 7 7 7 7 7 7 7 7 f . . . 
-        . . f 7 7 7 7 6 c 7 7 6 f c . . 
-        . . . f c c c c 7 7 6 f 7 7 c . 
-        . . c 7 2 7 7 7 6 c f 7 7 7 7 c 
-        . c 7 7 2 7 7 c f c 6 7 7 6 c c 
-        c 1 1 1 1 7 6 f c c 6 6 6 c . . 
-        f 1 1 1 1 1 6 6 c 6 6 6 6 f . . 
-        f 6 1 1 1 1 1 6 6 6 6 6 c f . . 
-        . f 6 1 1 1 1 1 1 6 6 6 f . . . 
-        . . c c c c c c c c c f . . . . 
-        `, SpriteKind.Enemy)
+    enm_1 = enmlist._pickRandom()
     for (let index = 0; index < randint(1, 2 * count); index++) {
-        tiles.placeOnTile(snake, tiles.getTileLocation(randint(0, 11), randint(0, 11)))
-        snake.follow(play1, randint(10, 50 + 5 * count))
+        tiles.placeOnTile(enm_1, tiles.getTileLocation(randint(0, 11), randint(0, 11)))
+        enm_1.follow(play1, randint(10, 50 + 5 * count))
         count2 += 1
     }
 }
-tileUtil.onMapLoaded(function (tilemap2) {
-    tileUtil.onMapLoaded(function (tilemap2) {
-    if (tiles.tileAtLocationEquals(tiles.getTileLocation(11, 6), sprites.dungeon.doorOpenEast)) {
-        lvl_1()
-    }
-    if (tiles.tileAtLocationEquals(tiles.getTileLocation(0, 5), sprites.dungeon.stairEast)) {
-        purpleGreen_1()
-    }
-    if (tiles.tileAtLocationEquals(tiles.getTileLocation(5, 11), sprites.dungeon.stairNorth)) {
-        purplePurple()
-    }
-    if (tiles.tileAtLocationEquals(tiles.getTileLocation(11, 5), sprites.dungeon.stairEast)) {
-        Grey()
-    }
-    if (tiles.tileAtLocationEquals(tiles.getTileLocation(11, 6), sprites.dungeon.stairWest)) {
-        purpleGreen_2()
-    }
-})
-})
 function purplePurple () {
     play1.setPosition(22, 88)
     tiles.setWallAt(tiles.getTileLocation(5, 11), false)
@@ -111,7 +93,9 @@ function purplePurple () {
 tileUtil.onMapLoaded(function (tilemap2) {
     enemy()
 })
+let enm_1: Sprite = null
 let tileList: tiles.TileMapData[] = []
+let enmlist: Sprite[] = []
 let TileMapLevel = 0
 let play1: Sprite = null
 let count = 0
@@ -141,6 +125,13 @@ scene.cameraFollowSprite(play1)
 let health_1 = statusbars.create(20, 4, StatusBarKind.Health)
 health_1.attachToSprite(play1)
 controller.moveSprite(play1, 100, 100)
+enmlist = [
+sprites.create(assets.image`enemy1`, SpriteKind.Player),
+sprites.create(assets.image`purplegreen2`, SpriteKind.Player),
+sprites.create(assets.image`purpgreen1`, SpriteKind.Player),
+sprites.create(assets.image`purplepurp`, SpriteKind.Player),
+sprites.create(assets.image`purpgrey`, SpriteKind.Player)
+]
 tileList = [
 tilemap`level7`,
 tilemap`level2`,
